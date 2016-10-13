3cca4413b6d16608bdff04fd28ad8e78c1fae46b
kraaas <570453516@qq.com>
2016-10-13 23:20:17 +0800

    update parser

--- src/parser.js
+++ src/parser.js
@@ -1,62 +1,45 @@
 import Watcher from './watcher'
 import { isNumber } from './util'
 
-const oprationReg = /([\+|-|\*|\/|%]\s*)(\b\w+\b)/g
-  // 表达式中允许的关键字
-var allowKeywords = 'Math.parseInt.parseFloat.Date.this.true.false.null.undefined.Infinity.NaN.isNaN.isFinite.decodeURI.decodeURIComponent.encodeURI.encodeURIComponent';
-var regAllowKeyword = new RegExp('^(' + allowKeywords.replace(/\./g, '\\b|') + '\\b)');
+// 表达式中允许的关键字
+const allowKeywords = 'Math.parseInt.parseFloat.Date.this.true.false.null.undefined.Infinity.NaN.isNaN.isFinite.decodeURI.decodeURIComponent.encodeURI.encodeURIComponent';
+const regAllowKeyword = new RegExp('^(' + allowKeywords.replace(/\./g, '\\b|') + '\\b)');
 
 // 表达式中禁止的关键字
-var avoidKeywords = 'var.const.let.if.else.for.in.continue.switch.case.break.default.function.return.do.while.delete.try.catch.throw.finally.with.import.export.instanceof.yield.await';
-var regAviodKeyword = new RegExp('^(' + avoidKeywords.replace(/\./g, '\\b|') + '\\b)');
+const avoidKeywords = 'var.const.let.if.else.for.in.continue.switch.case.break.default.function.return.do.while.delete.try.catch.throw.finally.with.import.export.instanceof.yield.await';
+const regAviodKeyword = new RegExp('^(' + avoidKeywords.replace(/\./g, '\\b|') + '\\b)');
 
 // 匹配常量缓存序号 "1"
-var regSaveConst = /"(\d+)"/g;
+const regSaveConst = /"(\d+)"/g;
 // 只含有 true 或 false
-var regBool = /^(true|false)$/;
+const regBool = /^(true|false)$/;
 // 匹配表达式中的常量
-var regReplaceConst = /[\{,]\s*[\w\$_]+\s*:|('[^']*'|"[^"]*")|typeof /g;
+const regReplaceConst = /[\{,]\s*[\w\$_]+\s*:|('[^']*'|"[^"]*")|typeof /g;
 // 匹配表达式中的取值域
-var regReplaceScope = /[^\w$\.]([A-Za-z_$][\w$]*(\.[A-Za-z_$][\w$]*|\['.*?'\]|\[".*?"\])*)/g;
+const regReplaceScope = /[^\w$\.]([A-Za-z_$][\w$]*(\.[A-Za-z_$][\w$]*|\['.*?'\]|\[".*?"\])*)/g;
 // 匹配常规取值: item or item['x'] or item["y"] or item[0]
-var regNormal = /^[A-Za-z_$][\w$]*(\.[A-Za-z_$][\w$]*|\['.*?'\]|\[".*?"\]|\[\d+\]|\[[A-Za-z_$][\w$]*\])*$/;
+const regNormal = /^[A-Za-z_$][\w$]*(\.[A-Za-z_$][\w$]*|\['.*?'\]|\[".*?"\]|\[\d+\]|\[[A-Za-z_$][\w$]*\])*$/;
 
-/**
- * 是否是常规指令表达式
- * @param   {String}   expression
- * @return  {Boolean}
- */
 function isNormal(expression) {
   return regNormal.test(expression) && !regBool.test(expression) && expression.indexOf('Math.') !== 0;
 }
 
 // 保存常量，返回序号 "i"
-var consts = [];
+let consts = [];
 
 function saveConst(string) {
-  var i = consts.length;
+  let i = consts.length;
   consts[i] = string;
   return '"' + i + '"';
 }
 
-/**
- * 返回替换之前的常量
- * @param   {Strinf}  string
- * @param   {Number}  i
- * @return  {String}
- */
 function returnConst(string, i) {
   return consts[i];
 }
 
-/**
- * 返回表达式的 scope 替换
- * @param   {String}  string
- * @return  {String}
- */
 function replaceScope(string) {
-  var pad = string.charAt(0);
-  var path = string.slice(1);
+  let pad = string.charAt(0);
+  let path = string.slice(1);
 
   if (regAllowKeyword.test(path)) {
     return string;
